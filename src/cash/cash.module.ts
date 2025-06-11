161f0cd845617dc3d08a29a0c6338aff99b641a9
Henry <henry8812@gmail.com>
2025-06-10 21:44:34 -0500

    calcs

--- src/cash/cash.module.ts
+++ src/cash/cash.module.ts
@@ -4,10 +4,12 @@ import { CashMovement } from 'src/entities/cash-movement.entity';
 import { AgentClosing } from 'src/entities/agent-closing.entity'; 
 import { CashService } from './cash.service';
 import { CashController } from './cash.controller';
+import { LoanRequestStatus } from 'src/entities/loan-request.entity';
+import { LoanTransaction } from 'src/entities/transaction.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([CashMovement, AgentClosing]),  
+    TypeOrmModule.forFeature([CashMovement, AgentClosing, LoanTransaction]),  
   ],
   providers: [CashService],
   controllers: [CashController],

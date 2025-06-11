83fe3c71a07276fcb1ac7dc93f84a514ed3546c9
Henry <henry8812@gmail.com>
2025-06-10 20:40:53 -0500

    package

--- src/cash/cash.module.ts
+++ src/cash/cash.module.ts
@@ -1,13 +1,16 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { CashMovement } from "src/entities/cash-movement.entity";
-import { CashController } from "./cash.controller";
-import { CashService } from "./cash.service";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CashMovement } from 'src/entities/cash-movement.entity';
+import { AgentClosing } from 'src/entities/agent-closing.entity'; 
+import { CashService } from './cash.service';
+import { CashController } from './cash.controller';
 
-// src/cash/cash.module.ts
 @Module({
-  imports: [TypeOrmModule.forFeature([CashMovement])],
-  controllers: [CashController],
+  imports: [
+    TypeOrmModule.forFeature([CashMovement, AgentClosing]),  
+  ],
   providers: [CashService],
+  controllers: [CashController],
+  exports: [CashService],
 })
 export class CashModule {}
